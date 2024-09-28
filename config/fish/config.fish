a3844980231ef06725da5b1b8a1f6d060f8791c4
dynnian <lian_dynn@pm.me>
2024-09-27 23:08:07 -0400

    Updated

---
+++ config/fish/config.fish
@@ -0,0 +1,244 @@
+##       ____             __
+##      / __ \_________ _/ /_____
+##     / / / / ___/ __ `/ //_/ _ \
+##    / /_/ / /  / /_/ / ,< /  __/  Clay Gomera (Drake)
+##   /_____/_/   \__,_/_/|_|\___/   My custom fish config
+##
+
+### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/.go/bin $HOME/.cargo/bin $HOME/.local/lib/flutter/bin $HOME/.local/lib/android/emulator $HOME/.local/share/JetBrains/Toolbox/scripts /var/lib/flatpak/exports/bin/ $fish_user_paths
+
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+
+### SET BAT AS MANPAGER
+set -x MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+### END OF VI MODE ###
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#504945'
+set fish_color_command brcyan
+set fish_color_error '#fb4934'
+set fish_color_param brcyan
+
+### FUNCTIONS ###
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# The bindings for !! and !$
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Function for copying files and directories, even recursively.
+# ex: copy DIRNAME LOCATIONS
+# result: copies the directory and all of its contents.
+function copy
+    set count (count $argv | tr -d \n)
+    if test "$count" = 2; and test -d "$argv[1]"
+	set from (echo $argv[1] | trim-right /)
+	set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
+end
+
+# Function for printing a column (splits input on whitespace)
+# ex: echo 1 2 3 | coln 3
+# output: 3
+function coln
+    while read -l input
+        echo $input | awk '{print $'$argv[1]'}'
+    end
+end
+
+# Function for printing a row
+# ex: seq 3 | rown 3
+# output: 3
+function rown --argument index
+    sed -n "$index p"
+end
+
+# Function for ignoring the first 'n' lines
+# ex: seq 10 | skip 5
+# results: prints everything but the first 5 lines
+function skip --argument n
+    tail +(math 1 + $n)
+end
+
+# Function for taking the first 'n' lines
+# ex: seq 10 | take 5
+# results: prints only the first 5 lines
+function take --argument number
+    head -$number
+end
+
+# unlock ssh keys
+function unlock
+    ssh-add "$HOME/.ssh/$argv[1]"
+end
+
+### END OF FUNCTIONS ###
+
+### ALIASES ###
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# use lunarvim or neovim for vim if present.
+if test -x "$HOME/.local/bin/lvim"
+    alias vim "$HOME/.local/bin/lvim"
+else if test -x (command -v nvim)
+    alias vim "nvim"
+end
+
+# bat as cat
+if test -x (command -v bat)
+    alias cat "bat"
+end
+
+# Changing "ls" to "eza"
+alias ls='eza -al --color=always --group-directories-first' # my preferred listing
+alias la='eza -a --color=always --group-directories-first'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first'  # long format
+alias lt='eza -aT --color=always --group-directories-first' # tree listing
+alias l.='eza -a | egrep "^\."'
+
+# package management
+alias pkg-update="sudo dnf update"
+alias pkg-install="sudo dnf install"
+alias pkg-remove="sudo dnf remove"
+alias pkg-search="sudo dnf search"
+alias pkg-clean="sudo dnf autoremove"
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# file management
+alias fm="yazi"
+alias flm="yazi"
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rm='rm -vI'
+alias mkd='mkdir -pv'
+alias mkdir='mkdir -pv'
+
+# audio
+alias mx='pulsemixer'
+alias amx='alsamixer'
+alias mk='musikcube'
+alias ms='musikcube'
+alias music='musikcube'
+
+# multimedia scripts
+alias fli='flix-cli'
+alias ani='ani-cli'
+alias aniq='ani-cli -q'
+
+# adding flags
+alias df='df -h'
+alias free='free -m'
+
+# ps
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+alias psmem='ps auxf | sort -nr -k 4'
+alias pscpu='ps auxf | sort -nr -k 3'
+
+# git
+alias git-adu='git add -u'
+alias git-adl='git add .'
+alias git-brn='git branch'
+alias git-chk='git checkout'
+alias git-cln='git clone'
+alias git-cmt='git commit -m'
+alias git-fth='git fetch'
+alias git-pll='git pull origin'
+alias git-psh='git push origin'
+alias git-sts='git status'
+alias git-tag='git tag'
+alias git-ntg='git tag -a'
+
+# power management
+alias po='systemctl poweroff'
+alias sp='systemctl suspend'
+alias rb='systemctl reboot'
+
+# youtube-
+alias yta-aac="yt-dlp --extract-audio --audio-format aac "
+alias yta-best="yt-dlp --extract-audio --audio-format best "
+alias yta-flac="yt-dlp --extract-audio --audio-format flac "
+alias yta-m4a="yt-dlp --extract-audio --audio-format m4a "
+alias yta-mp3="yt-dlp --extract-audio --audio-format mp3 "
+alias yta-opus="yt-dlp --extract-audio --audio-format opus "
+alias yta-vorbis="yt-dlp --extract-audio --audio-format vorbis "
+alias yta-wav="yt-dlp --extract-audio --audio-format wav "
+alias ytv-best="yt-dlp -f bestvideo+bestaudio "
+alias yt='ytfzf -ftslT chafa'
+alias youtube='ytfzf -ftslT chafa'
+alias ytm='ytfzf -mtslT chafa'
+alias youtube-music='ytfzf -mtslT chafa'
+
+# network and bluetooth
+alias netstats='nmcli dev'
+alias wfi='nmtui-connect'
+alias wfi-scan='nmcli dev wifi rescan && nmcli dev wifi list'
+alias wfi-edit='nmtui-edit'
+alias wfi-on='nmcli radio wifi on'
+alias wfi-off='nmcli radio wifi off'
+alias blt='bluetoothctl'
+
+# android emulator
+alias avd='QT_QPA_PLATFORM=xcb emulator -avd Pixel_7_Pro_API_35'
+alias avds='emulator -list-avds'
+alias avde='QT_QPA_PLATFORM=xcb emulator -avd'
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
+zoxide init fish | source
+clido completion fish | source
---
+++ home/.bash_profile
@@ -0,0 +1,123 @@
+# .bash_profile
+
+# Home XDG folders
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_LIB_HOME="$HOME/.local/lib"
+export XDG_BIN_HOME="$HOME/.local/bin"
+
+# Sanely export XDG Base dir variables
+eval "$(sed 's/^[^#].*/export &/g;t;d' ~/.config/user-dirs.dirs)"
+
+# Clean home
+export W3M_DIR="$XDG_DATA_HOME/w3m"
+export GTK2_RC_FILES="$HOME/.config/gtk-2.0/gtkrc-2.0"
+export WGETDIR="$XDG_CONFIG_HOME/wget"
+export WGETRC="$WGETDIR/wgetrc"
+export INPUTRC="$HOME/.config/X11/inputrc"
+export GNUPGHOME="$HOME/.local/share/gnupg"
+export LESSHISTFILE="-"
+export BASHRC="$HOME/.bashrc"
+
+# Default apps
+export TERMINAL="kitty"
+export EDITOR="$HOME/.local/bin/lvim"
+export VISUAL="zed"
+export BROWSER="flatpak run com.brave.Browser"
+export VIEWER="zathura"
+
+# Programming languages specific environment variables
+## Go
+export GOPATH="$XDG_DATA_HOME/go"
+
+## Rust
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+
+## Flutter
+export CHROME_EXECUTABLE="/var/lib/flatpak/app/com.brave.Browser/x86_64/stable/active/export/bin/com.brave.Browser"
+export PUB_CACHE="$XDG_DATA_HOME/pub-cache"
+export FLUTTER_ROOT="$XDG_LIB_HOME/flutter"
+export FLUTTER_ANALYTICS_DISABLED=true
+export ANALYZER_STATE_LOCATION_OVERRIDE="$XDG_CACHE_HOME/dartServer"
+
+## Android SDK/Emulator
+export ANDROID_HOME="$XDG_LIB_HOME/android"
+export ANDROID_SDK_ROOT="$ANDROID_HOME"
+export ANDROID_USER_HOME="$XDG_DATA_HOME/android"
+export ANDROID_EMULATOR_HOME="$ANDROID_USER_HOME"
+export ANDROID_AVD_HOME="$ANDROID_USER_HOME/avd"
+export JAVA_HOME="/usr/lib/jvm/jre-17-openjdk"
+export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
+
+# Set path
+## local bin paths
+if [ -d "$HOME/.bin" ]; then
+    PATH="$HOME/.bin:$PATH"
+fi
+if [ -d "$HOME/.local/bin" ]; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+## rust tools and programs
+if [ -d "$CARGO_HOME/bin" ]; then
+    PATH="$CARGO_HOME/bin:$PATH"
+fi
+
+## golang tools and programs
+if [ -d "$GOPATH/bin" ]; then
+    PATH="$GOPATH/bin:$PATH"
+fi
+
+## flutter sdk
+if [ -d "$FLUTTER_ROOT/bin" ]; then
+    PATH="$FLUTTER_ROOT/bin:$PATH"
+fi
+
+## android sdk
+if [ -d "$ANDROID_HOME/emulator" ]; then
+    PATH="$ANDROID_HOME/emulator:$PATH"
+fi
+if [ -d "$ANDROID_HOME/cmdline-tools/latest/bin" ]; then
+    PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
+fi
+if [ -d "$ANDROID_HOME/tools" ]; then
+    PATH="$ANDROID_HOME/tools:$PATH"
+fi
+if [ -d "$ANDROID_HOME/tools/bin" ]; then
+    PATH="$ANDROID_HOME/tools/bin:$PATH"
+fi
+
+## dotnet sdk tools
+if [ -d $DOTNET_CLI_HOME/tools ]; then
+    PATH="$DOTNET_CLI_HOME/tools:$PATH"
+fi
+
+## AppImage applications
+if [ -d "$HOME/Applications" ]; then
+    PATH="$HOME/Applications:$PATH"
+fi
+
+## jetbrains toolbox
+if [ -d "$XDG_DATA_HOME/JetBrains/Toolbox/scripts" ]; then
+    PATH="$XDG_DATA_HOME/JetBrains/Toolbox/scripts:$PATH"
+fi
+
+# Create config directories if they don't exist
+if [ ! -d "$WGETDIR" ] || [ ! -d "$GNUPGHOME" ]; then
+    mkdir -p "$WGETDIR" "$GNUPGHOME"
+fi
+
+# export SSH_AUTH_SOCK
+if [[ -z "${SSH_CONNECTION}" ]]; then
+    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+fi
+
+# Bashrc
+source "$BASHRC"
+
+# Starting Hyprland
+if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
+    exec Hyprland &>/dev/null
+    logout
+fi
